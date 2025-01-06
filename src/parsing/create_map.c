e56269fbb9575f1a98e9337a89a8ca120f48516d
qvan-ste <accounts@vanstenus.com>
2025-01-06 19:36:36 +0100

    created main and create_map.c

---
+++ src/parsing/create_map.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   create_map.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: qvan-ste <qvan-ste@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/06 17:29:30 by qvan-ste      #+#    #+#                 */
+/*   Updated: 2025/01/06 19:30:48 by qvan-ste      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "../../libs/libft/include/libft.h"
+#include "../../include/cub3D.h"
+
+static int	create_map_vector(t_vector_ptr *map_vector, int map_fd)
+{
+	char			*buf;
+
+	buf = get_next_line(map_fd);
+	if (!buf)
+		return (-1);
+	if (ft_vecinit_ptr(map_vector, 10, sizeof(char *)) == -1)
+		return (-1);
+	while (buf)
+	{
+		if (buf[ft_strlen(buf) - 1] == '\n')
+			buf[ft_strlen(buf) - 1] = '\0';
+		if (ft_vecappend_ptr(map_vector, buf) == -1)
+		{
+			free(buf);
+			ft_vecdelete_ptr(map_vector);
+			return (-1);
+		}
+		buf = get_next_line(map_fd);
+	}
+	return (0);
+}
+
+static char	**create_map_matrix(t_vector_ptr *map_vector)
+{
+	size_t	i;
+	char	**map_matrix;
+
+	i = 0;
+	map_matrix = ft_calloc(map_vector -> size + 1, sizeof(char *));
+	if (!map_matrix)
+		return (NULL);
+	while (i < map_vector -> size)
+	{
+		map_matrix[i] = ft_vecget_ptr(map_vector, i);
+		i++;
+	}
+	return (map_matrix);
+}
+
+int	create_map(char *file, t_map_data *map_data)
+{
+	int				map_fd;
+	t_vector_ptr	map_vector;
+
+	map_fd = open(file, O_RDONLY);
+	if (map_fd == -1)
+	{
+		perror("Cub3D");
+		return (1);
+	}
+	if (create_map_vector(&map_vector, map_fd) == -1)
+	{
+		perror("Cub3D");
+		close(map_fd);
+		return (1);
+	}
+	map_data -> map = create_map_matrix(&map_vector);
+	close(map_fd);
+	free(map_vector.items);
+	if (!map_data -> map)
+		return (1);
+	return (SUCCESS);
+}
--- src/utils.c
+++ src/utils.c
@@ -1,35 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   utils.c                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: qvan-ste <qvan-ste@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2025/01/06 16:12:48 by qvan-ste      #+#    #+#                 */
-/*   Updated: 2025/01/06 19:35:53 by qvan-ste      ########   odam.nl         */
+/*   Created: 2025/01/06 17:35:12 by qvan-ste      #+#    #+#                 */
+/*   Updated: 2025/01/06 17:37:29 by qvan-ste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libs/libft/include/libft.h"
-#include "../include/cub3D.h"
 
-int	main(int argc, char **argv)
+int	check_extension(char *arg)
 {
-	t_map_data	map_data;
+	char	*last_four_chars;
 
-	if (argc != 2)
-	{
-		ft_printf_fd(2, "Error\nIncorrect number of argments\n");
-		return (1);
-	}
-	if (check_extension(argv[1]) != 0)
-	{
-		ft_printf_fd(2, "Error\nIncorrect file format\n");
-		return (1);
-	}
-	if (create_map(argv[1], &map_data) != SUCCESS)
-		return (1);
-	ft_print_matrix(map_data .map);
-	ft_free_matrix(map_data.map);
-	return (0);
+	last_four_chars = &arg[ft_strlen(arg) - 4];
+	return (ft_strcmp(".cub", last_four_chars));
 }
