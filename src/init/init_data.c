31d1e80ffec28091a105a31ca163aef7d463caaa
qvan-ste <accounts@vanstenus.com>
2025-02-10 11:32:26 +0100

    added viewdirections

--- src/init/init_data.c
+++ src/init/init_data.c
@@ -6,7 +6,7 @@
 /*   By: qvan-ste <qvan-ste@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/15 21:50:19 by qvan-ste      #+#    #+#                 */
-/*   Updated: 2025/01/22 18:42:35 by qvan-ste      ########   odam.nl         */
+/*   Updated: 2025/02/10 11:30:27 by qvan-ste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,56 @@ static t_player	*init_player(int player_x, int player_y)
 	return (player);
 }
 
+static void	set_view_direction(int direction, t_camera	*camera)
+{
+	if (direction == WEST)
+	{
+		camera->plane_x = 0;
+		camera->plane_y = 0.66;
+	}
+	else if (direction == EAST)
+	{
+		camera->plane_x = 0;
+		camera->plane_y = -0.66;
+	}
+	else if (direction == NORTH)
+	{
+		camera->plane_x = -0.66;
+		camera->plane_y = 0;
+	}
+	else if (direction == SOUTH)
+	{
+		camera->plane_x = 0.66;
+		camera->plane_y = 0;
+	}
+}
+
 static t_camera	*init_camera(int view_direction_x, int view_direction_y)
 {
 	t_camera	*camera;
+	int			direction;
 
 	camera = malloc(sizeof(t_camera));
 	if (!camera)
 	{
 		return (NULL);
 	}
-	camera->plane_x = 0;
-	camera->plane_y = 0.66;
 	camera->view_dir_x = view_direction_x;
 	camera->view_dir_y = view_direction_y;
+	if (view_direction_x == -1 && view_direction_y == 0)
+		direction = WEST;
+	else if (view_direction_x == 1 && view_direction_y == 0)
+		direction = EAST;
+	else if (view_direction_x == 0 && view_direction_y == -1)
+		direction = NORTH;
+	else if (view_direction_x == 0 && view_direction_y == 1)
+		direction = SOUTH;
+	else
+	{
+		free (camera);
+		return (NULL);
+	}
+	set_view_direction(direction, camera);
 	return (camera);
 }
 
@@ -61,7 +98,7 @@ int	init_data(t_data *game, char *map_file)
 	game->player = init_player(3, 4);
 	if (!game->player)
 		return (FAILURE);
-	game->camera = init_camera(-1, 0);
+	game->camera = init_camera(0, 1);
 	if (!game->camera)
 		return (FAILURE);
 	game->display = init_display(texture_paths, rgb_floor, rgb_ceiling);
